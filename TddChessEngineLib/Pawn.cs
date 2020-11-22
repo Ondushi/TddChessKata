06dbe4f7dbbf6a1ce03ad94e7c9d9b632e7cb730
Ondushi <a_pyagay@mail.ru>
2020-11-22 11:57:46 +0300

    of

--- TddChessEngineLib/Pawn.cs
+++ TddChessEngineLib/Pawn.cs
@@ -4,48 +4,21 @@ namespace TddChessEngineLib
 {
     public class Pawn
     {
-        public Position Pos {get; set; }
-        public string Color {get; set; }
-        public Pawn(string position, string color)
+        public Position Pos {get; private set; }
+        public FigureColor FigureColor {get;}
+        public Pawn(string position, FigureColor figureColor)
         {
             Pos = new Position(position);
-            if (color == "Black" || color == "White")
-            {
-                Color = color;
-            }
-            else
-            {
-                Color = "White";
-            }
+            FigureColor = figureColor;
         }
         public void TurnTo(string finishPos)
         {
             Position finishPosition = new Position(finishPos);
-            if (Pos.Letter == finishPosition.Letter)
-            {
-                if (Color == "White")
-                {
-                    if (Pos.Number < finishPosition.Number && (finishPosition.Number - Pos.Number) <= 2)
-                    {
-                        Pos = finishPosition;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Impossible move");
-                    }
-                }
-                else
-                {
-                    if (Pos.Number > finishPosition.Number && (Pos.Number - finishPosition.Number) <= 2)
-                    {
-                        Pos = finishPosition;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Impossible move");
-                    }
-                }
-            }
+            Pos = finishPosition;
         }
+    }
+    public enum FigureColor
+    {
+        White, Black
     }
 }
